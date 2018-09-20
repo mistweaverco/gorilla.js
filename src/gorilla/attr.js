ae9fb492b669e34c92479961d9780e59cdd4605b
Marco Kellershoff <hello@gorilla.moe>
2018-09-20 22:32:54 +0200

    Add `.attr()` and `.data()` query by JSON list

--- src/gorilla/attr.js
+++ src/gorilla/attr.js
@@ -2,12 +2,23 @@ gorilla.attr = function(el, key, value) {
         "use strict";
         let returnvalue;
         returnvalue = el;
-        if (value) {
-                el.setAttribute(key, value);
-        } else if (typeof key === "object" && !key.length) {
-                gorilla.each(key, (v, k) => {
-                        el.setAttribute(k, v);
-                });
+        if (typeof value !== "undefined") {
+                if (typeof value === "object") {
+                        value = JSON.stringify(value);
+                }
+                el.setAttribute(key, String(value));
+        } else if (typeof key === "object") {
+                if (key.length) {
+                        let list = {};
+                        gorilla.each(key, (k) => {
+                                list[k] = el.getAttribute(k);
+                        });
+                        returnvalue = list;
+                } else {
+                        gorilla.each(key, (v, k) => {
+                                el.setAttribute(k, String(v));
+                        });
+                }
         } else {
                 returnvalue = el.getAttribute(key);
         }
--- src/gorilla/data.js
+++ src/gorilla/data.js
@@ -2,18 +2,26 @@ gorilla.data = function(el, key, value) {
         "use strict";
         let returnvalue;
         returnvalue = el;
-        if (value) {
+        if (typeof value !== "undefined") {
                 if (typeof value === "object") {
                         value = JSON.stringify(value);
                 }
-                el.setAttribute("data-" + key, value);
-        } else if (typeof key === "object" && !key.length) {
-                gorilla.each(key, (v, k) => {
-                        if (typeof v === "object") {
-                                v = JSON.stringify(v);
-                        }
-                        el.setAttribute("data-" + k, v);
-                });
+                el.setAttribute("data-" + key, String(value));
+        } else if (typeof key === "object") {
+                if (key.length) {
+                        let list = {};
+                        gorilla.each(key, (k) => {
+                                list[k] = el.getAttribute("data-" + k);
+                        });
+                        returnvalue = list;
+                } else {
+                        gorilla.each(key, (v, k) => {
+                                if (typeof v === "object") {
+                                        v = JSON.stringify(v);
+                                }
+                                el.setAttribute("data-" + k, String(v));
+                        });
+                }
         } else {
                 returnvalue = el.getAttribute("data-" + key);
         }
