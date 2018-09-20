3c17b24eeda6194e816b586fd9149a683ce4c22c
Marco Kellershoff <hello@gorilla.moe>
2018-09-20 16:13:47 +0200

    Make `.each()` also work with objects

--- src/gorilla/each.js
+++ src/gorilla/each.js
@@ -1,9 +1,17 @@
 gorilla.each = function(arr, cb) {
         "use strict";
-        let i = 0;
-        let len = arr.length;
-        for (; i < len; i++) {
-                cb(arr[i], i);
+        if (arr.length) {
+                let i = 0;
+                let len = arr.length;
+                for (; i < len; i++) {
+                        cb(arr[i], i);
+                }
+        } else {
+                for (let k in arr) {
+                        if (Reflect.has(arr, k)) {
+                                cb(arr[k], k);
+                        }
+                }
         }
         return arr;
 };
